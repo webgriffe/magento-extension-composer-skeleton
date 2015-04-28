288bba8db8f0604eee50e5d99a7a81df86c30ea9
Manuele Menozzi <manuele.menozzi@gmail.com>
2015-04-28 18:30:13 +0200

    Adds wip skeleton

---
+++ src/app/code/community/Vendor/Module/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manuele
+ * Date: 28/04/15
+ * Time: 18:20
+ */ 
+class Vendor_Module_Helper_Data extends Mage_Core_Helper_Abstract {
+
+}
---
+++ src/app/code/community/Vendor/Module/sql/vendor_module_setup/install-1.0.0.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: manuele
+ * Date: 28/04/15
+ * Time: 18:20
+ */ 
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+
+
+
+$installer->endSetup();
