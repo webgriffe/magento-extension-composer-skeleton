bf2b20e52b9ee026b4ab085003a2c58dea1c1cc0
Manuele Menozzi <manuele.menozzi@gmail.com>
2015-04-29 09:40:17 +0200

    wip setup

--- setup.sh
+++ setup.sh
@@ -31,48 +31,52 @@ for i in $(find ${SCRIPT_DIR} -name "Vendor")
 do
 	dir=$(dirname "$i")
 	echo "$i -> $dir/${VENDOR_NAME}"
+	mv $i "$dir/${VENDOR_NAME}"
 done
 
 for i in $(find ${SCRIPT_DIR} -name "Module")
 do
         dir=$(dirname "$i")
         echo "$i -> $dir/${MODULE_NAME}"
+	mv $i "$dir/${MODULE_NAME}"
 done
 
 for i in $(find ${SCRIPT_DIR} -name "Vendor_Module.xml")
 do
         dir=$(dirname "$i")
         echo "$i -> $dir/${VENDOR_NAME}_${MODULE_NAME}.xml"
+	mv $i "$dir/${VENDOR_NAME}_${MODULE_NAME}.xml"
 done
 
 for i in $(find ${SCRIPT_DIR} -name "vendor_module_setup")
 do
         dir=$(dirname "$i")
         echo "$i -> $dir/${VENDOR_NAME_LOWER}_${MODULE_NAME_LOWER}_setup"
+	mv $i "$dir/${VENDOR_NAME_LOWER}_${MODULE_NAME_LOWER}_setup"
 done
 
 for i in $(find ${SCRIPT_DIR} -name "*.php")
 do
 	echo "Replacing 'Vendor_Module' with '${VENDOR_NAME}_${MODULE_NAME}' in $i..."
-	#sed -i 's/Vendor_Module/${VENDOR_NAME}_${MODULE_NAME}/g' $i
+	sed -i '.original' 's/Vendor_Module/${VENDOR_NAME}_${MODULE_NAME}/g' $i
 done
 
 for i in $(find ${SCRIPT_DIR} -name "*.xml")
 do
         echo "Replacing 'Vendor_Module' with '${VENDOR_NAME}_${MODULE_NAME}' in $i..."
-        #sed -i 's/Vendor_Module/${VENDOR_NAME}_${MODULE_NAME}/g' $i
+        sed -i '.original' 's/Vendor_Module/${VENDOR_NAME}_${MODULE_NAME}/g' $i
 done
 
 for i in $(find ${SCRIPT_DIR} -name "*.xml")
 do
         echo "Replacing 'vendor_module' with '${VENDOR_NAME_LOWER}_${MODULE_NAME_LOWER}' in $i..."
-        #sed -i 's/vendor_module/${VENDOR_NAME_LOWER}_${MODULE_NAME_LOWER}/g' $i
+        sed -i '.original' 's/vendor_module/${VENDOR_NAME_LOWER}_${MODULE_NAME_LOWER}/g' $i
 done
 
 file="${SCRIPT_DIR}/modman"
 echo "Replacing 'Vendor_Module' with '${VENDOR_NAME}_${MODULE_NAME}' in $file..."
-#sed -i 's/Vendor_Module/${VENDOR_NAME}_${MODULE_NAME}/g' $file
+sed -i '.original' 's/Vendor_Module/${VENDOR_NAME}_${MODULE_NAME}/g' $file
 
 file="${SCRIPT_DIR}/composer.json"
 echo "Replacing 'module/vendor' with '${COMPOSER_NAME}' in $file..."
-#sed -i 's/module\/vendor/${COMPOSER_NAME}/g' $file
+sed -i '.original' 's|vendor/module|webgriffe/store-redirect|g' $file
