1c7f29aab3f43109c51ce2739223f4b4e5b4bbd0
Manuele Menozzi <manuele.menozzi@gmail.com>
2015-04-29 19:58:27 +0200

    Rimuove commento da classi php

--- src/app/code/community/Vendor/Module/Helper/Data.php
+++ src/app/code/community/Vendor/Module/Helper/Data.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: manuele
- * Date: 28/04/15
- * Time: 18:20
- */ 
-class Vendor_Module_Helper_Data extends Mage_Core_Helper_Abstract {
 
+class Vendor_Module_Helper_Data extends Mage_Core_Helper_Abstract
+{
 }
--- src/app/code/community/Vendor/Module/data/vendor_module_setup/data-install-1.0.0.php
+++ src/app/code/community/Vendor/Module/data/vendor_module_setup/data-install-1.0.0.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: manuele
- * Date: 28/04/15
- * Time: 18:20
- */ 
+
 /* @var $installer Mage_Core_Model_Resource_Setup */
 $installer = $this;
 
